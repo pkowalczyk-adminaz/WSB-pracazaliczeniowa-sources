e31aee83b78041c381a7595aa6b6c9b6c82bbf77
Pawel <it.pkowalczyk@gmail.com>
2023-11-15 20:02:37 +0100

    fdgdfg

--- VM-v02.ps1
+++ VM-v02.ps1
@@ -8,7 +8,7 @@ $networkname = Read-Host -Prompt "Podaj nazwe sieci"
 $dnsLabelPrefix = Read-Host -Prompt "Enter an unique DNS name for the public IP" 
 
 
-#Connect-AzAccount -Tenant $tenantID
+Connect-AzAccount -Tenant $tenantID
 New-AzResourceGroup -Name $resourceGroupName -Location "$location" 
 New-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
